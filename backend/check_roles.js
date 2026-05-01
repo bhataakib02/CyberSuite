d00727d159251d3b6678b45c89ef34ce60020686
Mohammad Aakib Bhat <bhataakib02@gmail.com>
2026-05-01 11:26:53 +0530

    removed redudent files

--- backend/check_roles.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function main() {
-  const roles = await prisma.$queryRaw`SELECT DISTINCT role FROM "User"`;
-  console.log('Roles in DB:', roles);
-}
-
-main().catch(console.error).finally(() => prisma.$disconnect());
